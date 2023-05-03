c0a5e786493854c323be8637bb789f75b34ed8ee
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-03 09:37:23 -0300

    adding default mode

--- src/components/ContactSection/index.jsx
+++ src/components/ContactSection/index.jsx
@@ -12,27 +12,27 @@ const ContactSection = () => {
       <div className="container mx-auto">
         <Title title="Contact Me" />
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4  mt-4 sm:mt-8">
-          <div className="bg-gray-700 rounded-lg shadow-lg p-6">
+          <div className="bg-white dark:bg-gray-700 rounded-lg shadow-lg p-6">
             <div className="flex items-center mb-4">
               <HiOutlineMail className="h-6 w-6 text-[#00e1fe] mr-4" />
-              <span className="text-white font-medium">Email:</span>
+              <span className="text-gray-700 dark:text-white font-medium">Email:</span>
             </div>
             <Link
               hrefLink="mailto:ailinndev@gmail.com"
               link="ailinndev@gmail.com"
             />
           </div>
-          <div className="bg-gray-700 rounded-lg shadow-lg p-6">
+          <div className="bg-white dark:bg-gray-700 rounded-lg shadow-lg p-6">
             <div className="flex items-center mb-4">
               <FaTelegram className="h-6 w-6 text-[#00e1fe] mr-4" />
-              <span className="text-white font-medium">Phone:</span>
+              <span className="text-gray-700 dark:text-white font-medium">Phone:</span>
             </div>
             <Link hrefLink="https://t.me/ailinndev" link="@ailinndev" />
           </div>
-          <div className="bg-gray-700 rounded-lg shadow-lg p-6">
+          <div className="bg-white dark:bg-gray-700 rounded-lg shadow-lg p-6">
             <div className="flex items-center mb-4">
               <FaLinkedin className="h-6 w-6 text-[#00e1fe] mr-4" />
-              <span className="text-white font-medium">LinkedIn:</span>
+              <span className="text-gray-700 dark:text-white font-medium">LinkedIn:</span>
             </div>
             <Link
               hrefLink="https://www.linkedin.com/in/ailinak/"
