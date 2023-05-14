593b40177fbebb6e57d43485ed9cb9f0c6321b2d
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-14 17:12:42 -0300

    adding lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import Project from "./components/Project";
-import LanguageSection from "./components/LanguageSection";
-import ProfileSection from "./components/ProfileSection";
-import Banner from "./components/Banner";
-import SkillsSection from "./components/SkillsSection";
-import ExperienceSection from "./components/ExperienceSection";
-import Footer from "./components/Footer";
-import ContactSection from "./components/ContactSection";
-import EducationSection from "./components/EducationSection";
+import { lazy, Suspense } from "react";
 import { useMobile } from "./hooks/useMobile";
 import { user } from "./data/user";
+
+const Banner = lazy(() => import("./components/Banner"));
+const ProfileSection = lazy(() => import("./components/ProfileSection"));
+const ExperienceSection = lazy(() => import("./components/ExperienceSection"));
+const EducationSection = lazy(() => import("./components/EducationSection"));
+const Project = lazy(() => import("./components/Project"));
+const SkillsSection = lazy(() => import("./components/SkillsSection"));
+const LanguageSection = lazy(() => import("./components/LanguageSection"));
+const ContactSection = lazy(() => import("./components/ContactSection"));
+const Footer = lazy(() => import("./components/Footer"));
 
 const App = () => {
   const isMobile = useMobile();
 
   return (
     <div className="container mx-auto p-4 bg-white dark:bg-gray-900 md:bg-transparent md:dark:bg-transparent ">
-      {!isMobile && <Banner />}
-      <ProfileSection user={user} />
-      <ExperienceSection />
-      <EducationSection />
-      <Project />
-      <SkillsSection />
-      <LanguageSection />
-      <ContactSection />
-      <Footer />
+      <Suspense fallback={<div>Loading...</div>}>
+        {!isMobile && <Banner />}
+        <ProfileSection user={user} />
+        <ExperienceSection />
+        <EducationSection />
+        <Project />
+        <SkillsSection />
+        <LanguageSection />
+        <ContactSection />
+        <Footer />
+      </Suspense>
     </div>
   );
 };
