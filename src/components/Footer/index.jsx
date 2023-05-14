ee2f160a0a72f3f437f2f034710e6198563bdf4f
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-14 10:46:36 -0300

    Adding footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="bg-white dark:bg-gray-900 py-8 rounded-b-lg">
+    <footer className="py-8 rounded-b-lg  bg-white dark:bg-gray-900 shadow-md">
       <div className="max-w-screen-xl mx-auto px-8">
         <div className="flex justify-center">
           <div className="text-gray-700 dark:text-gray-300 text-sm ">
