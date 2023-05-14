71f7674c2b87888fdfe2881eedc459a2b2a45c9d
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-14 17:48:46 -0300

    Loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,3 +1,4 @@
+// Loader.js
 import { motion } from "framer-motion";
 
 const Loader = () => {
@@ -13,24 +14,11 @@ const Loader = () => {
   };
 
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-      }}
-    >
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <motion.div
         variants={loaderVariants}
         animate="animate"
-        style={{
-          width: "50px",
-          height: "50px",
-          border: "5px solid #ccc",
-          borderTopColor: "#00bcd4",
-          borderRadius: "50%",
-        }}
+        className="w-12 h-12 border-4 border-blue-600 dark:border-[#00e1fe] border-t-4 rounded-full animate-spin bg-white dark:bg-gray-900"
       />
     </div>
   );
