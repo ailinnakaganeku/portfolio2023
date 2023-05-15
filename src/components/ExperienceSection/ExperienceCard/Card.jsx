396076ca913f5eae46ccb50b248c76ad93064ab8
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-15 15:28:16 -0300

    What Alek said

--- src/components/ExperienceSection/ExperienceCard/Card.jsx
+++ src/components/ExperienceSection/ExperienceCard/Card.jsx
@@ -17,7 +17,7 @@ const Card = (experience) => {
           className="relative"
         >
           <span className="pb-1"> {company} </span>
-          <span className="absolute bg-gray-800 dark:bg-[#00e1fe] h-px left-0 right-0 bottom-0"></span>
+          <span className="absolute bg-gray-800 dark:bg-[#00e1fe] left-0 right-0 h-1 bottom-[-1px]"></span>
         </a>
       </h3>
       <div className="flex items-center mb-4">
