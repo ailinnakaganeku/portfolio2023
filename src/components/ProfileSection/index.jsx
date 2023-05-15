03d9bcee8ab81b757e947a27419a72b46d2ffd0e
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-15 00:30:19 -0300

    Refactor code

--- src/components/ProfileSection/index.jsx
+++ src/components/ProfileSection/index.jsx
@@ -82,7 +82,7 @@ const ProfileSection = () => {
           <div
             className="h-40 md:h-60 bg-cover bg-top relative rounded-t-lg"
             style={{
-              backgroundImage: `url('https://img.freepik.com/free-photo/desktop-with-computer_23-2148110268.jpg?w=1800&t=st=1683118997~exp=1683119597~hmac=ec3bc482cfb62025dcd1a101edd8db6605471ebd52b91687ed9bf8fe795dc80b')`,
+              backgroundImage: `url('/banner.webp')`,
             }}
           >
             <div className="absolute inset-0 bg-black opacity-50 dark:bg-gray-900 dark:opacity-60 rounded-t-lg"></div>
