02a773a4a3084bc7aee092c215939033994a79c4
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-20 14:23:08 -0300

    Animation bug - fixed

--- src/components/ProfileSection/index.jsx
+++ src/components/ProfileSection/index.jsx
@@ -138,7 +138,7 @@ const ProfileSection = () => {
             <Link
               to="contact"
               smooth={true}
-              duration={700}
+              duration={500}
               onClick={() => toggleAnimation("contact")}
             >
               <button className="font-medium px-4 gap-4 w-40 h-10 dark:bg-opacity-10 bg-opacity-80 bg-gray-100 border dark:border-[#00e1fe] border-blue-600 rounded text-blue-600 flex dark:text-[#00e1fe] items-center justify-center font-size-14">
@@ -149,7 +149,7 @@ const ProfileSection = () => {
             <Link
               to="projects"
               smooth={true}
-              duration={700}
+              duration={500}
               onClick={() => toggleAnimation("projects")}
             >
               <button className="font-medium px-4 gap-4 w-40 h-10 dark:bg-opacity-10 bg-opacity-80 bg-gray-100 border border-gray-900 dark:border-white rounded dark:text-white flex items-center justify-center whitespace-nowrap font-size-14">
