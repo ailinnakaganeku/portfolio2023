c704a347ec5dfa72410058ea43b664b42c9a2fcb
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-20 09:04:40 -0300

    fix languages animation

--- src/components/LanguageSection/LanguageCard/index.jsx
+++ src/components/LanguageSection/LanguageCard/index.jsx
@@ -1,11 +1,13 @@
 const LanguageCard = (language) => {
   const { name, level } = language;
   return (
-    <div className="flex items-center justify-between">
-      <div className="text-gray-700 dark:text-white font-bold">{name}</div>
-      <div className="flex items-center">
-        <div className="bg-blue-600 dark:bg-[#00e1fe] h-2 w-8 rounded-md mr-2"></div>
-        <div className="text-gray-500 dark:text-gray-300">{level}</div>
+    <div className="bg-white dark:bg-gray-700 rounded-lg shadow-lg p-4">
+      <div className="flex items-center justify-between">
+        <div className="text-gray-700 dark:text-white font-bold">{name}</div>
+        <div className="flex items-center">
+          <div className="bg-blue-600 dark:bg-[#00e1fe] h-2 w-8 rounded-md mr-2"></div>
+          <div className="text-gray-500 dark:text-gray-300">{level}</div>
+        </div>
       </div>
     </div>
   );
--- src/components/LanguageSection/index.jsx
+++ src/components/LanguageSection/index.jsx
@@ -14,7 +14,6 @@ const LanguageSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4 sm:mt-6">
         {languages?.map((language) => (
           <div
-            className="bg-white dark:bg-gray-700 rounded-lg shadow-lg p-4"
             key={language.name}
           >
             {isMobile ? (
