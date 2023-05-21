f7e748a455b5e7fb3df6f90e7f89fb20bf2168e2
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-21 10:21:46 -0300

    lang

--- src/data/languages.js
+++ src/data/languages.js
@@ -1,6 +1,6 @@
 export const languages = [
   { name: "Spanish", level: "Native" },
-  { name: "English", level: "Fluent" },
-  { name: "Portuguese", level: "Beginner" },
-  { name: "German", level: "Beginner" },
+  { name: "English", level: "Advance C1" },
+  { name: "Portuguese", level: "Beginner A2" },
+  { name: "German", level: "Beginner A1" },
 ];
