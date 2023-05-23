e87ccc3b48756cf4c369f9642b6a203d93273a02
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-23 10:40:56 -0300

    bg in light mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Component from "./components/Component";
 
 const App = () => {
   return (
-    <div className="container mx-auto px-6 py-4 bg-white md:bg-gray-100 dark:bg-gray-900 md:bg-transparent md:dark:bg-transparent">
-      <Suspense fallback={<Loader />}>
-        <Component />
-      </Suspense>
+    <div className="bg-white md:bg-gray-50 dark:bg-gray-900  md:dark:bg-transparent">
+      <div className="container mx-auto px-6 py-4 bg-white md:bg-gray-50 dark:bg-gray-900 md:dark:bg-transparent">
+        <Suspense fallback={<Loader />}>
+          <Component />
+        </Suspense>
+      </div>
     </div>
   );
 };
