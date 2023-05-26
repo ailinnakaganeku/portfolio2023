078414ae5430cd80d3c1c2650a1c236582e9ddd0
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-05-26 07:21:49 -0300

    Fix padding & sizes

--- src/components/SkillsSection/SkillCard/index.jsx
+++ src/components/SkillsSection/SkillCard/index.jsx
@@ -1,7 +1,7 @@
 const SkillCard = ({ image, name }) => {
   return (
-    <div className="bg-white dark:bg-gray-700 p-2 rounded-md shadow-md flex flex-col items-center justify-center">
-      <img src={image} alt={name} className="h-8 w-8 mb-2" />
+    <div className="bg-white dark:bg-gray-700 p-2 md:p-4 rounded-md shadow-md flex flex-col items-center justify-center">
+      <img src={image} alt={name} className="h-6 w-6 md:h-8 md:w-8 mb-2" />
       <div className="text-md font-medium dark:text-white">{name}</div>
     </div>
   );
